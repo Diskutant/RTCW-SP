eab51d62b68ee0bf2311e70e7f3858ba2db9f2a5
Jonathan Gray <jsg@jsg.id.au>
2013-03-10 21:12:09 +1100

    use strlcpy/strlcat when dealing with paths in extractfuncs

--- src/extractfuncs/extractfuncs.c
+++ src/extractfuncs/extractfuncs.c
@@ -152,7 +152,7 @@ static char *func_filedesc = "g_func_decs.h";
 
 void DumpReplaceFunctions( void ) {
 	replacefunc_t *rf;
-	char path[_MAX_PATH];
+	char path[PATH_MAX];
 	FILE    *f;
 	int len, newlen;
 	unsigned char *buf, *newbuf;
@@ -161,9 +161,9 @@ void DumpReplaceFunctions( void ) {
 	updated = 0;
 
 	// dump the function header
-	strcpy( path, "." );
-	strcat( path, PATHSEPERATOR_STR );
-	strcat( path, "g_funcs.tmp" );
+	strlcpy( path, ".", sizeof(path) );
+	strlcat( path, PATHSEPERATOR_STR, sizeof(path) );
+	strlcat( path, "g_funcs.tmp", sizeof(path) );
 	Log_Open( path );
 	for ( rf = replacefuncs; rf; rf = rf->next )
 	{
@@ -173,7 +173,7 @@ void DumpReplaceFunctions( void ) {
 	Log_Close();
 
 	// if it's different, rename the file over the real header
-	strcpy( path, "g_funcs.tmp" );
+	strlcpy( path, "g_funcs.tmp", sizeof(path) );
 	f = fopen( path, "rb" );
 	fseek( f, 0, SEEK_END );
 	len = ftell( f );
@@ -183,7 +183,7 @@ void DumpReplaceFunctions( void ) {
 	buf[len] = 0;
 	fclose( f );
 
-	strcpy( path, func_filename );
+	strlcpy( path, func_filename, sizeof(path) );
 	if ( f = fopen( path, "rb" ) ) {
 		fseek( f, 0, SEEK_END );
 		newlen = ftell( f );
@@ -194,13 +194,13 @@ void DumpReplaceFunctions( void ) {
 		fclose( f );
 
 		if ( len != newlen || Q_stricmp( buf, newbuf ) ) {
-			char newpath[_MAX_PATH];
+			char newpath[PATH_MAX];
 
 			// delete the old file, rename the new one
-			strcpy( path, func_filename );
+			strlcpy( path, func_filename, sizeof(path) );
 			remove( path );
 
-			strcpy( newpath, "g_funcs.tmp" );
+			strlcpy( newpath, "g_funcs.tmp", sizeof(newpath) );
 			rename( newpath, path );
 
 			// make g_save recompile itself
@@ -212,7 +212,7 @@ void DumpReplaceFunctions( void ) {
 			updated = 1;
 		} else {
 			// delete the old file
-			strcpy( path, "g_funcs.tmp" );
+			strlcpy( path, "g_funcs.tmp", sizeof(path) );
 			remove( path );
 		}
 	} else {
@@ -223,7 +223,7 @@ void DumpReplaceFunctions( void ) {
 	free( newbuf );
 
 	// dump the function declarations
-	strcpy( path, "g_func_decs.tmp" );
+	strlcpy( path, "g_func_decs.tmp", sizeof(path) );
 	Log_Open( path );
 	for ( rf = replacefuncs; rf; rf = rf->next )
 	{
@@ -232,7 +232,7 @@ void DumpReplaceFunctions( void ) {
 	Log_Close();
 
 	// if it's different, rename the file over the real header
-	strcpy( path, "g_func_decs.tmp" );
+	strlcpy( path, "g_func_decs.tmp", sizeof(path) );
 	f = fopen( path, "rb" );
 	fseek( f, 0, SEEK_END );
 	len = ftell( f );
@@ -242,7 +242,7 @@ void DumpReplaceFunctions( void ) {
 	buf[len] = 0;
 	fclose( f );
 
-	strcpy( path, func_filedesc );
+	strlcpy( path, func_filedesc, sizeof(path) );
 	if ( f = fopen( path, "rb" ) ) {
 		fseek( f, 0, SEEK_END );
 		newlen = ftell( f );
@@ -253,13 +253,13 @@ void DumpReplaceFunctions( void ) {
 		fclose( f );
 
 		if ( len != newlen || Q_stricmp( buf, newbuf ) ) {
-			char newpath[_MAX_PATH];
+			char newpath[PATH_MAX];
 
 			// delete the old file, rename the new one
-			strcpy( path, func_filedesc );
+			strlcpy( path, func_filedesc, sizeof(path) );
 			remove( path );
 
-			strcpy( newpath, "g_func_decs.tmp" );
+			strlcpy( newpath, "g_func_decs.tmp", sizeof(path) );
 			rename( newpath, path );
 
 			// make g_save recompile itself
@@ -272,7 +272,7 @@ void DumpReplaceFunctions( void ) {
 			updated = 1;
 		} else {
 			// delete the old file
-			strcpy( path, "g_func_decs.tmp" );
+			strlcpy( path, "g_func_decs.tmp", sizeof(path) );
 			remove( path );
 		}
 	} else {
